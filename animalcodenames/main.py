c5307f33a8f2f489c47fcd6d86db303dfffa6117
noahtren <noahtren@gmail.com>
2019-08-27 15:41:23 -0400

    first commit

---
+++ animalcodenames/main.py
@@ -0,0 +1,25 @@
+import os
+import random
+
+adjectives = None
+animals = None
+colors = None
+
+def gen_name():
+  install_location = os.path.abspath(os.path.dirname(__file__))
+
+  global adjectives, animals, colors
+  if adjectives is None:
+    adjectives = open(os.path.join(install_location, "adjectives.txt"), "r").read().split("\n")
+    animals = open(os.path.join(install_location, "animals.txt"), "r").read().split("\n")
+    colors = open(os.path.join(install_location, "colors.txt"), "r").read().split("\n")
+    assert "" not in adjectives
+    assert "" not in animals
+    assert "" not in colors
+    print("read files")
+
+  name = '-'.join([random.choice(adjectives),
+                  random.choice(colors),
+                  random.choice(animals),
+                  str(random.randint(1, 99))])
+  return name
